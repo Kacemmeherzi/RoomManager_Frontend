15dc35de78ff1885f5b5e6df2019ee67c3132c97
kacem <kacemmeherzi52@gmail.com>
2024-03-16 08:10:52 +0100

    routes updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RoomComponent } from './room/room.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'rooms', component: RoomComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
