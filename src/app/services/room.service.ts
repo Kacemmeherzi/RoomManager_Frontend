b4130804bcc061476dac198a7bb8177eb05fe69b
kacem <kacemmeherzi52@gmail.com>
2024-03-16 08:07:04 +0100

    room service created

---
+++ src/app/services/room.service.ts
@@ -0,0 +1,42 @@
+// room.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Room } from '../models/room.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomService {
+  private apiUrl = 'http://localhost:3000/room';
+
+  constructor(private http: HttpClient) {}
+
+  // Get all rooms
+  getRooms(): Observable<Room[]> {
+    return this.http.get<Room[]>(this.apiUrl);
+  }
+
+  // Get room by ID
+  getRoom(id: number): Observable<Room> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<Room>(url);
+  }
+
+  // Add new room
+  addRoom(room: Room): Observable<Room> {
+    return this.http.post<Room>(this.apiUrl, room);
+  }
+
+  // Update room
+  updateRoom(room: Room): Observable<Room> {
+    const url = `${this.apiUrl}/${room.id}`;
+    return this.http.put<Room>(url, room);
+  }
+
+  // Delete room
+  deleteRoom(id: number): Observable<Room> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<Room>(url);
+  }
+}
