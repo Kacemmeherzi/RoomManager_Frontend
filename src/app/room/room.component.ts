410c99a92c16163bde9de49f471ed0b0b5a50762
kacem <kacemmeherzi52@gmail.com>
2024-03-16 08:09:13 +0100

    room component added

---
+++ src/app/room/room.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Room } from '../models/room.model';
+import { RoomService } from '../services/room.service';
+import AuthService from '../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-room',
+  templateUrl: './room.component.html',
+  styleUrl: './room.component.css'
+})
+export class RoomComponent {
+ 
+  rooms?: Room[];
+
+  constructor(private roomService: RoomService,  private authService: AuthService, private router : Router) {}
+ 
+ 
+  ngOnInit(): void {
+    this.getRooms();
+  }
+  isLoggedIn(): boolean {
+    return this.authService.logged;
+  }
+  getRooms(): void {
+    this.roomService.getRooms()
+      .subscribe(rooms => this.rooms = rooms);
+      console.log(this.rooms)
+  }
+}
