f595ca9b8c0b9c481f165db7e0c2e4b3066721c4
kacem <kacemmeherzi52@gmail.com>
2024-03-20 02:46:57 +0100

    regustration method added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,13 +2,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export default class AuthService {
-   private  apiUrl = 'http://localhost:3000/auth/login';
+   private  apiUrl = 'http://localhost:3000/auth/';
       logged : boolean = false   ;
+      user? : User    ;
 
   constructor(private http: HttpClient) {}
   isAuthenticated(): boolean {
@@ -22,9 +24,24 @@ export default class AuthService {
 
     const body = { username, password };
 
-    return this.http.post(this.apiUrl, body, { headers ,observe: 'response'}).pipe(
+    return this.http.post(this.apiUrl+"login", body, { headers ,observe: 'response'}).pipe(
       catchError(error => {
         console.error('Error during login', error);
+        return throwError(error);
+      })
+    );
+  }
+  register(username: string, password: string, email : string):Observable<HttpResponse<any>> {
+    
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    const body = { username, password ,email };
+
+    return this.http.post(this.apiUrl+"register", body, { headers ,observe: 'response'}).pipe(
+      catchError(error => {
+        console.error('Error during registration', error);
         return throwError(error);
       })
     );
