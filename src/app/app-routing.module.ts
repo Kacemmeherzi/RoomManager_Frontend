38229785b1c63bb29c1c76f1f52528081ae97d5f
kacem <kacemmeherzi52@gmail.com>
2024-04-06 11:30:34 +0200

    make reservation route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RoomComponent } from './room/room.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { MakereservationComponent } from './makereservation/makereservation.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'rooms', component: RoomComponent },
-  { path: 'register', component: RegistrationComponent }
+  { path: 'register', component: RegistrationComponent },
+  {path : 'makereservation', component :MakereservationComponent}
 
 ];
 
