c55cf437f77552203536297de6493a7e4a89e2f3
kacem <kacemmeherzi52@gmail.com>
2024-04-24 16:43:41 +0200

    new routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,20 @@ import { HomeComponent } from './home/home.component';
 import { RoomComponent } from './room/room.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { MakereservationComponent } from './makereservation/makereservation.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'rooms', component: RoomComponent },
   { path: 'register', component: RegistrationComponent },
-  {path : 'makereservation', component :MakereservationComponent}
+  {path : 'makereservation', component :MakereservationComponent},
+  {path : 'reservation', component :ReservationComponent}
+  , {path : 'calendar' , component : CalendarComponent},
+  {path : 'registration' , component :RegistrationComponent}
+
+  
 
 ];
 
